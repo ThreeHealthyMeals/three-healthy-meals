de6b590e024d9ebaae9fb42929611f518a27281a
Unknown <cupjoo@naver.com>
2020-11-02 21:41:36 +0900

    [food] add Food, Ingestion

---
+++ src/main/java/com/example/threehealthymeals/domain/food/IngestionRepository.java
@@ -0,0 +1,6 @@
+package com.example.threehealthymeals.domain.food;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngestionRepository extends JpaRepository<Ingestion, Long> {
+}
