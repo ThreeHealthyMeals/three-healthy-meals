33eadc8474844e74375cd1fc0b485d6132fd420c
cupjoo <cupjoo@naver.com>
2020-11-12 00:22:44 +0900

    [restaurant] add restaurant simple, detail responses

--- src/main/java/com/example/threehealthymeals/domain/restaurant/MenuRepository.java
+++ src/main/java/com/example/threehealthymeals/domain/restaurant/MenuRepository.java
@@ -3,4 +3,6 @@ package com.example.threehealthymeals.domain.restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    Menu getByRestaurant(Restaurant restaurant);
 }
