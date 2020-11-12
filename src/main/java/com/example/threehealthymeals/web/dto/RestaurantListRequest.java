0f08cf8063908e64cda97e611198471d3b5b5a49
cupjoo <cupjoo@naver.com>
2020-11-12 16:58:08 +0900

    [restaurant] implement restaurant query repository

--- src/main/java/com/example/threehealthymeals/web/dto/RestaurantListRequest.java
+++ src/main/java/com/example/threehealthymeals/web/dto/RestaurantListRequest.java
@@ -9,5 +9,12 @@ import lombok.Setter;
 @Getter
 public class RestaurantListRequest {
 
-    private int price;
+    private String keyword;         // 키워드
+    private String negativeKeyword; // 제외 식단 키워드
+    private boolean coronaSafe;     // 코로나 안심식당
+    private String latitude;        // 위도
+    private String longitude;       // 경도
+    private int maxPrice;           // 가격대
+    private boolean activityData;   // 내 활동량
+    private boolean calorie;        // 내 섭취 칼로리
 }
