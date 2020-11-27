a9cf6b5f7aa580d4afbf10daf97b568b21c899fa
ch33233 <jchcaksj2@gmail.com>
2020-11-27 11:03:34 +0900

    add dic

--- crawler/nutrition_crawler.py
+++ crawler/nutrition_crawler.py
@@ -1,13 +1,14 @@
 from urllib.request import urlopen
+from urllib.request import HTTPError
+from urllib.request import URLError
 from bs4 import BeautifulSoup
 from pprint import pprint
 import requests
 import urllib.parse
 import re
 
-mainUrl = 'https://www.fatsecret.kr/'
 
-# 메뉴 검색
+mainUrl = 'https://www.fatsecret.kr/'
 
 def searchMenu():
     menuName = input('검색어를 입력하세요 : ')
@@ -20,13 +21,36 @@ def removeTag(text_origin):
 
     return text_removeTag
 
+def open_url(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    except URLError as e:
+        return None
+
+def removeBrackets(text):
+    text = text[1:len(text)-1]
+
+    return text
+
+def StringToList(string):
+    string = string.split(', ')
+
+    return string
+
+
+# 메뉴 검색
 
 baseUrl = 'https://www.fatsecret.kr/%EC%B9%BC%EB%A1%9C%EB%A6%AC-%EC%98%81%EC%96%91%EC%86%8C/search?q='
 plusUrl = searchMenu()
-
+plusUrl = '불타는 짜장면'
 url = baseUrl + urllib.parse.quote_plus(plusUrl)
 
+open_url(url)
+
 html = requests.get(url)
+
 soup = BeautifulSoup(html.text, 'html.parser')
 
 search_a_tag = soup.find('a', {'class':'prominent'})
@@ -39,27 +63,48 @@ search_url = mainUrl + search_link
 html = requests.get(search_url)
 soup = BeautifulSoup(html.text, 'html.parser')
 
+
 """
 # 영양정보 전체 크롤링
 
 title = soup.find('h1', {'style':'text-transform:none'})
-print(removeTag(title))
+removeTag(title)
+
+print(title)
 
 nutrition = soup.find('div', {'class':'nutrition_facts international'})
 nutrition = removeTag(nutrition)
 
-pprint(nutrition)
+print(nutrition)
 """
+
 
 # 영양정보 크롤링 리스트
 
+
+# 서빙 사이즈
+
+serving_size = soup.find('div', {'class':'serving_size black serving_size_label'})
+serving_size_amount = soup.find('div', {'class':'serving_size black serving_size_value'})
+
+serving_size = removeTag(serving_size)
+serving_size_amount = removeTag(serving_size_amount)
+
+print(serving_size + serving_size_amount)
+"""
+# 영양소
+
 nutrition_list = soup.find_all('div', {'class':'nutrient black left'})
 nutrition_sublist = soup.find_all('div', {'class':'nutrient sub left'})
+nutrition_list2 = soup.find_all('div', {'class':'nutrient left'})
 
 nutrition_list = removeTag(nutrition_list)
 nutrition_sublist = removeTag(nutrition_sublist)
+nutrition_list2 = removeTag(nutrition_list2)
 
-pprint(nutrition_list + nutrition_sublist)
+print(nutrition_list + nutrition_sublist + nutrition_list2)
+"""
+# 영양소 함유량
 
 nutrition_list_amount = soup.find_all('div', {'class':'nutrient black right tRight'})
 nutrition_sublist_amount = soup.find_all('div', {'class':'nutrient right tRight'})
@@ -67,9 +112,24 @@ nutrition_sublist_amount = soup.find_all('div', {'class':'nutrient right tRight'
 nutrition_list_amount = removeTag(nutrition_list_amount)
 nutrition_sublist_amount = removeTag(nutrition_sublist_amount)
 
-pprint(nutrition_list_amount + nutrition_sublist_amount)
+nutrition_list_amount = removeBrackets(nutrition_list_amount)
+nutrition_sublist_amount = removeBrackets(nutrition_sublist_amount)
 
-"""
-div serachNoResult 예외처리
-dictionary형태
-"""
+amount_list = StringToList(nutrition_list_amount)
+amount_sublist = StringToList(nutrition_sublist_amount)
+
+amount_list.extend(amount_sublist)
+
+print(amount_list)
+
+
+# 딕셔너리형태 저장
+
+nut_list = ['열량(kJ)', '탄수화물', '단백질', '지방', '열량(kcal)', '설탕당', '포화지방', '트랜스지방', '다불포화지방', '불포화지방', '콜레스테롤', '식이섬유', '나트륨', '칼륨']
+#nut_amount_list = ['3286 kJ', '129.41g', '26.81g', '19.98g', '785 kcal', '6.56g', '5.621g', '0g', '4.043g', '8.517g', '22mg', '13.8g', '600mg', '803mg']
+nut_dict = {}
+nut_amount_list = amount_list
+
+nut_dict = dict(zip(nut_list, nut_amount_list))
+
+print(nut_dict)
